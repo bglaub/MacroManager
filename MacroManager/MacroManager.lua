a361035490030d3361833e30d6cafae5335362b2
Blake Glaub <blakeglaub@gmail.com>
2024-01-06 13:09:33 -0600

    feat: add main frame

--- MacroManager/MacroManager.lua
+++ MacroManager/MacroManager.lua
@@ -1,11 +1,25 @@
 local ADDON_NAME = "MacroManager"
 
-local frame = CreateFrame("FRAME");
+-- Create the main frame that will contain the macro manager
+local macro_manager_main_frame = CreateFrame("FRAME", "MacroManagerMainFrame", UIParent, "BasicFrameTemplateWithInset");
 
-frame:RegisterEvent("ADDON_LOADED");
-frame:RegisterEvent("PLAYER_LOGOUT");
+-- Set all the UI factors of the main frame
+macro_manager_main_frame:SetMovable(true);
+macro_manager_main_frame:EnableMouse(true);
+macro_manager_main_frame:RegisterForDrag("LeftButton");
+macro_manager_main_frame:SetScript("OnDragStart", macro_manager_main_frame.StartMoving);
+macro_manager_main_frame:SetScript("OnDragStop", macro_manager_main_frame.StopMovingOrSizing);
+macro_manager_main_frame:SetWidth(1000);
+macro_manager_main_frame:SetHeight(500);
+macro_manager_main_frame:SetPoint("CENTER");
+macro_manager_main_frame:Hide();
 
-frame:SetScript("OnEvent", function(self, event, addon)
+-- Establish the events that the main frame should be concerned with
+macro_manager_main_frame:RegisterEvent("ADDON_LOADED");
+macro_manager_main_frame:RegisterEvent("PLAYER_LOGOUT");
+
+-- Establish the event handler for the main frame
+macro_manager_main_frame:SetScript("OnEvent", function(self, event, addon)
   if event == "ADDON_LOADED" and addon == ADDON_NAME then
     MacroManagerDataAccessor:Initialize();
   elseif event == "PLAYER_LOGOUT" then
@@ -15,6 +29,7 @@ end);
 
 SLASH_MACROMANAGER1 = "/macromanager";
 function SlashCmdList.MACROMANAGER(msg)
+  macro_manager_main_frame:Show();
   ------------------------------ BEGIN: TEST CODE ------------------------------
   if MacroManagerData.count == nil then
     MacroManagerData.count = 0;
